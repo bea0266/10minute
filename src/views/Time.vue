3a20a6c1ee81658345e6645836837f82fb2995f2
bea0266 <bea0266@naver.com>
2023-03-12 00:37:05 +0900

    스톱워치 리셋 기능 추가

--- src/views/Time.vue
+++ src/views/Time.vue
@@ -17,13 +17,13 @@
         </div>
 
         <button type="button" @click="stopTimer()">STOP</button>
-        <button type="button">RESET</button>
+        <button type="button" @click="reset()">RESET</button>
         <button type="button" @click="startTimer()">START</button>
     </div>
 </template>
 <script setup>
 import { ref, computed, watch } from 'vue';
-import { useSettingStore } from '@/store/setting';
+import { useSettingStore } from '@/store/setting.js';
 const settingStore = useSettingStore();
 
 const off = ref(true)
@@ -31,20 +31,24 @@ const todayStudyTime = ref(settingStore.settingStudyTime);
 const overTime = ref('00:00:00');
 const realStudyTime = ref('00:00:00');
 const todayStudyInterval = ref();
+const overStudyInterval = ref();
 const realStudyInterval = ref();
 
 function startTimer() {
-    todayStudyInterval.value = setInterval(() => {
-         let [hour, min, second] = todayStudyTime.value.split(':');
+    if(off.value) {
+        todayStudyInterval.value = setInterval(() => {
+        let [hour, min, second] = todayStudyTime.value.split(':');
          
-         if (parseInt(second) < 10) {
+        if (parseInt(second) < 10) {
             
             if(parseInt(second) === 0) {
                 second = 59;
+
                 if(min > 0) {
                     min = parseInt(min) - 1;
                 } else {
                     min = 59;
+
                     if(hour > 0) {
                         hour = parseInt(hour) - 1
                     }
@@ -74,21 +78,64 @@ function startTimer() {
 
          todayStudyTime.value = [hour, min, second].join(':');
        
-    }, 1000)
+      }, 1000)      
+    } else {
+        startOverTime();
+    }
+
 }
 
-function reStartTimer() {
-    return;   
+function startOverTime() {
+    overStudyInterval.value = setInterval(() => {
+        let [hour, minute, seconds] = overTime.value.split(':');
+        
+        seconds = parseInt(seconds) + 1;
+        
+        if(seconds >= 60) {
+            seconds = 0;
+            minute = parseInt(minute) + 1; 
+            
+            if(minute === 60) {
+                minute = 0;
+                hour = parseInt(hour) + 1;    
+            }
+        }
+
+        if(hour < 10) {
+           hour = addZero(hour);
+         }
+         
+         if(minute < 10) {
+           minute = addZero(minute);
+         }
+         
+         if(seconds < 10) {
+           seconds = addZero(seconds);
+         }
+
+         overTime.value = [hour, minute, seconds].join(':');
+
+    }, 1000)
+
     
 }
 
 function stopTimer() {
-    clearInterval(todayStudyInterval.value);  
+    clearInterval(todayStudyInterval.value);
+    clearInterval(overStudyInterval.value);  
+}
+
+function reset() {
+    overTime.value = '00:00:00';
+    off.value = true;
+    todayStudyTime.value = settingStore.settingStudyTime;
 }
 
 function addZero(digit) {
     return '0' + parseInt(digit);   
 }
+
+
 
 watch(() => todayStudyTime.value, () => {
     if(todayStudyTime.value === '00:00:00') {
@@ -99,6 +146,8 @@ watch(() => todayStudyTime.value, () => {
         const overTimeArea = document.querySelectorAll('.time_area')[1];
 
         overTimeArea.style.color = 'red';
+        
+        startOverTime();
     }
 });
 
